cecbe35b5d8023bf5fff929979bdfdfd065b339c
Greg Cosgrove <gregcosgrove@gmail.com>
2017-08-15 20:30:02 -0400

    Complete project

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBO4WyZAWYB4NzazPBUfgm6BPPXkDbXY_A",
+  authDomain: "catch-of-the-day-greg-cosgrove.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-greg-cosgrove.firebaseio.com",
+});
+
+export default base;
